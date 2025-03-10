31428925710b22beb63022f96f1fc5aa510585f7
TEDDER130 <96477833+TEDDER130@users.noreply.github.com>
2025-03-11 00:07:55 +0100

    Update update.php
    awt

--- update.php
+++ update.php
@@ -1,7 +1,7 @@
 <?php
 $allowed_key = "enzo";
 
-if (!isset($_GET['key']) || $_GET['key'] !== $allowed_password) {
+if (!isset($_GET['key']) || $_GET['key'] !== $allowed_key) {
     die("<h1 style='color: blue; text-align: center;'>ACCESS DENIED YOUR IP WILL BE SEND TO THE FBI AND THEY WILL AREST YOU UNDER LAW OR SMTH IDK</h1>");
 }
 ?>
