06303622285a6c6675d053d2941e79fead7fe796
Ruben-Haverkamp <r.haverkamp2@student.han.nl>
2025-03-11 09:05:09 +0100

    normaliecss

--- update.php
+++ update.php
@@ -13,6 +13,7 @@ if (!isset($_GET['key']) || $_GET['key'] !== $allowed_key) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Update Website</title>
+    <link rel="stylesheet" href="normalize.css">
     <link rel="stylesheet" href="styles.css">
     <style>
         body {
