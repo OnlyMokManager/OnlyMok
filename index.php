893ab551b24901e88ab0537b038fe8316322e334
TEDDER130 <96477833+TEDDER130@users.noreply.github.com>
2025-03-11 19:16:14 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,9 @@
  if ($_SERVER['REQUEST_METHOD'] == 'POST') {
      $username = $_POST['username'];
      $password = $_POST['password'];
- 
+    echo 'Username: ' . $username . '<br>';
+    echo 'Password: ' . $password . '<br>';
+
      $user = $connect->prepare('SELECT id, username, [password] FROM users WHERE username = :username');
      $user->execute(['username' => $username]);
  
